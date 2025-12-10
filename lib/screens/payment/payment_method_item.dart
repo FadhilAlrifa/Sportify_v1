713a17d7f28661dac35d3c86bdb0ef78c61a2eac
aabiilrqm <abilarqam0@gmail.com>
2025-12-10 11:01:02 +0800

    Menambahkan metode pembayaran pada sistem booking

--- lib/screens/payment/payment_method_item.dart
+++ lib/screens/payment/payment_method_item.dart
@@ -3,29 +3,42 @@ import 'package:flutter/material.dart';
 class PaymentMethodItem extends StatelessWidget {
   final String label;
   final IconData icon;
+  final bool selected;
+  final VoidCallback onTap;
 
   const PaymentMethodItem({
     super.key,
     required this.label,
     required this.icon,
+    required this.selected,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onTap,
       child: Container(
         padding: const EdgeInsets.all(14),
-        margin: const EdgeInsets.only(bottom: 10),
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey.shade300),
+          border: Border.all(
+            color: selected ? Colors.blue : Colors.grey.shade300,
+            width: selected ? 2 : 1,
+          ),
           borderRadius: BorderRadius.circular(12),
         ),
         child: Row(
           children: [
-            Icon(icon, size: 28, color: Colors.blueGrey),
+            Icon(icon,
+                size: 28, color: selected ? Colors.blue : Colors.blueGrey),
             const SizedBox(width: 15),
-            Text(label, style: const TextStyle(fontSize: 16)),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: selected ? FontWeight.bold : FontWeight.normal,
+              ),
+            ),
           ],
         ),
       ),
